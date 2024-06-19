a042db08ced5f4c83a3d5b0929f54f9264c41f5a
samandareo <smdeveloper04@gmail.com>
2024-06-19 21:58:10 +0500

    change some parts

--- sqlDB.py
+++ sqlDB.py
@@ -1,18 +1,32 @@
 import pysqlite3 as sqlite3
 
-def create_database_and_table():
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
+# def create_database_and_table():
+#     conn = sqlite3.connect('users.db')
+#     cursor = conn.cursor()
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS responded_users (
-        user_id INTEGER PRIMARY KEY
-    )
-    ''')
+#     cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS responded_users (
+#         user_id INTEGER PRIMARY KEY
+#     )
+#     ''')
 
-    conn.commit()
-    conn.close()
+#     conn.commit()
+#     conn.close()
 
-if __name__ == "__main__":
-    create_database_and_table()
-    print("Database and table created successfully.")
+# def create_new_db():
+#     conn = sqlite3.connect('config.db')
+#     cursor = conn.cursor()
+
+#     cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS information (
+#         response_id INTEGER PRIMARY KEY,
+#         response_text TEXT
+#     )
+#     ''')
+
+#     conn.commit()
+#     conn.close()
+
+# if __name__ == "__main__":
+#     create_new_db()
+#     print("Database and table created successfully.")
--- userbot.py
+++ userbot.py
@@ -10,6 +10,8 @@ from telethon.tl.functions.contacts import ImportContactsRequest
 from telethon.tl.functions.contacts import GetContactsRequest
 import pytz
 import datetime
+from transformers import pipeline
+import requests
 
 # Own imports
 from credentials import API_ID, API_HASH, PHONE_NUMBER
@@ -20,6 +22,7 @@ api_id = API_ID
 api_hash = API_HASH
 phone_number = PHONE_NUMBER
 client = TelegramClient('anon', api_id, api_hash)
+sentiment_pipeline = pipeline("sentiment-analysis")
 global is_stopped
 is_stopped = False
 
@@ -27,6 +30,37 @@ is_stopped = False
 conn =  sqlite3.connect('database/users.db')
 cursor = conn.cursor()
 
+
+def translate_text(text):
+    url = "https://deep-translator-api.azurewebsites.net/google/"
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    params = {
+        "source": "uz",
+        "target": "en",
+        "text": text,
+        "proxies": []
+    }
+
+    response = requests.post(url, data=json.dumps(params), headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['translation']
+    else:
+        return None
+
+
+
+def is_positive_response(message):
+
+    message = translate_text(message)
+    results = sentiment_pipeline(message)
+
+    if results[0]['label'] == 'POSITIVE' and results[0]['score'] > 0.75:
+        return True
+    return False
 
 async def send_message_to_contact(contact_phone, name, message):
     # For adding contact to the contact list (This is main part of the code)
@@ -38,18 +72,17 @@ async def send_message_to_contact(contact_phone, name, message):
 
     target_user = next((user for user in contacts.users if user.phone == contact_phone), None)
 
-    if target_user:
+    if target_user and target_user.id != None:
         await client.send_message(target_user, message)
         print(f"Message sent to {target_user.first_name}")
-        time = datetime.datetime.now(pytz.timezone('Asia/Tashkent'))
-        await client.send_message(-4110173191, f"Message sent to {target_user.first_name}\nTime : {time}!")
+        await client.send_message(-4110173191, f"Message sent to {target_user}\nTime : {datetime.datetime.now(pytz.timezone('Asia/Tashkent'))}!")
         client.action(target_user, 'typing')
         await asyncio.sleep(3)
         await client.send_message(target_user, f'{name} aka , sizga ushbu bayramona sovg\'alarimiz haqida ma\'lumot bersam maylimi?')
 
     else:
         print("Contact not found.")
-        await client.send_message(-4110173191, f"{target_user.first_name} is not found!\nTime : {time}!")
+        await client.send_message(-4110173191, f"User is not found!\nTime : {datetime.datetime.now(pytz.timezone('Asia/Tashkent'))}!")
         return None
     
 
@@ -65,7 +98,7 @@ async def start_conversation():
         await send_message_to_contact(contact[1], contact[0], f"Assalomu alaykum {contact[0]} aka!\n\nIsmim Sevara. Siz avval ‘Strawberry House’ kompaniyamizning mahsulotidan xarid qilgan ekansiz. Shu sababli Hayit ayyomi✨ munosabati bilan, biz qadrdon mijozlarimizga, o'z yaqinlarini bayram arafasida xursand qilishlari uchun maxsus chegirmalar bermoqdamiz🎉!")
         cnt += 1
         database.move_specific_row()
-        await asyncio.sleep(120)
+        await asyncio.sleep(15)
         global is_stopped
         if is_stopped:
             break
@@ -91,19 +124,26 @@ async def handler(event):
         cursor.execute('DELETE FROM responded_users')
         conn.commit()
         await event.reply("Mijozlar ro'yxati tozalandi.")
-    elif not event.is_group:
+    if not event.is_group:
         user_id = event.sender_id
-        cursor.execute('SELECT user_id FROM responded_users WHERE user_id = ?', (user_id,))
-        if cursor.fetchone() is None:
-            async with client.action(event.chat_id, 'typing'):
-                await asyncio.sleep(5)
-                await event.reply(f'Sovg\'alar uchun mo\'ljallangan chegirmadagi mahsulotlarimiz bilan, quiydagi **"[Havola](http://t.me/StrawebarryHouse_bot/store)"**ni bosing!\n\n')
-                cursor.execute('INSERT INTO responded_users (user_id) VALUES (?)', (user_id,))
-                conn.commit()
+        if is_positive_response(event.raw_text):
+            cursor.execute('SELECT user_id FROM responded_users WHERE user_id = ?', (user_id,))
+            if cursor.fetchone() is None:
+                async with client.action(event.chat_id, 'typing'):
+                    await asyncio.sleep(3)
+                    await event.reply(f'Sovg\'alar uchun mo\'ljallangan chegirmadagi mahsulotlarimiz bilan, quiydagi **"[Havola](http://t.me/StrawebarryHouse_bot/store)"**ni bosing!\n\n')
+                    cursor.execute('INSERT INTO responded_users (user_id) VALUES (?)', (user_id,))
+                    conn.commit()
+            else:
+                print(f"{user_id} ga javob berilgan")
         else:
-            print(f"{user_id} ga javob berilgan")
+            print("Negative response")
+            await event.reply(f'Axa tushinarli, agarda mahsulotlarimizni yaqinlaringizga sovg\'a sifatida xadya qilishni hohlasangiz, **"[LAUNCH](http://t.me/StrawebarryHouse_bot/store)"** knopkasini bosish orqali sotuv botimizdan foydalanishingiz mumkin!')
+            cursor.execute('INSERT INTO responded_users (user_id) VALUES (?)', (user_id,))
+            conn.commit()
+    else:
+        print("Unknown command")
 
 
-client.start()
-client.run_until_disconnected()
+
 
