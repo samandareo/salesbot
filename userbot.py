c6e56db84428b9f0950109b86756990e469b14d7
samandareo <smdeveloper04@gmail.com>
2024-07-02 18:09:29 +0500

    update without model

--- userbot.py
+++ userbot.py
@@ -10,11 +10,10 @@ from telethon.tl.functions.contacts import ImportContactsRequest
 from telethon.tl.functions.contacts import GetContactsRequest
 import pytz
 import datetime
-from transformers import pipeline
 import requests
 
 # Own imports
-from credentials import API_ID, API_HASH, PHONE_NUMBER
+from credentials import API_ID, API_HASH, PHONE_NUMBER, MODEL_API_HEADERS
 import pysqlite3 as sqlite3
 import database
 
@@ -22,7 +21,10 @@ api_id = API_ID
 api_hash = API_HASH
 phone_number = PHONE_NUMBER
 client = TelegramClient('anon', api_id, api_hash)
-sentiment_pipeline = pipeline("sentiment-analysis")
+
+# This model help check score of sentiment
+model_api_url = "https://api-inference.huggingface.co/models/distilbert/distilbert-base-uncased-finetuned-sst-2-english"
+model_api_headers = MODEL_API_HEADERS
 global is_stopped
 is_stopped = False
 
@@ -56,9 +58,9 @@ def translate_text(text):
 def is_positive_response(message):
 
     message = translate_text(message)
-    results = sentiment_pipeline(message)
-
-    if results[0]['label'] == 'POSITIVE' and results[0]['score'] > 0.75:
+    results = requests.post(model_api_url, headers=model_api_headers, json=message)
+    results = results.json()
+    if results[0][0]['label'] == 'POSITIVE' and results[0][0]['score'] > 0.75:
         return True
     return False
 
@@ -75,10 +77,7 @@ async def send_message_to_contact(contact_phone, name, message):
     if target_user and target_user.id != None:
         await client.send_message(target_user, message)
         print(f"Message sent to {target_user.first_name}")
-        await client.send_message(-4110173191, f"Message sent to {target_user}\nTime : {datetime.datetime.now(pytz.timezone('Asia/Tashkent'))}!")
-        client.action(target_user, 'typing')
-        await asyncio.sleep(3)
-        await client.send_message(target_user, f'{name} aka , sizga ushbu bayramona sovg\'alarimiz haqida ma\'lumot bersam maylimi?')
+        await client.send_message(-4110173191, f"Message sent to {target_user.first_name}\nTime : {datetime.datetime.now(pytz.timezone('Asia/Tashkent'))}!")
 
     else:
         print("Contact not found.")
@@ -95,7 +94,7 @@ async def start_conversation():
         print("Conversation ended")
         return None
     for contact in contacts:
-        await send_message_to_contact(contact[1], contact[0], f"Assalomu alaykum {contact[0]} aka!\n\nIsmim Sevara. Siz avval ‘Strawberry House’ kompaniyamizning mahsulotidan xarid qilgan ekansiz. Shu sababli Hayit ayyomi✨ munosabati bilan, biz qadrdon mijozlarimizga, o'z yaqinlarini bayram arafasida xursand qilishlari uchun maxsus chegirmalar bermoqdamiz🎉!")
+        await send_message_to_contact(contact[1], contact[0], f'Assalomu alaykum {contact[0]}\n\nUmid qilamanki, mahsulotimiz sizga manzur kelgan!\n\nBizning kompaniyamiz mahsulotlaridan xarid qilganingizga bir yil bo\'ldi. Sizga, qayta buyurtma berishingiz uchun eng to\'g\'ri va maqbul vaqt ekanligini eslatmoqchimiz. Sababi ushbu haftada, bir yillik munosabati bilan, qayta buyurtma qilingan barcha mahsulotlarimiz uchun maxsus chegirmalar taklif qilamiz.\n\nSizga qulaylik yaratish maqsadida, buyurtma berishni soddalashtirdik☺️\n\nQuyidagi havolani bosish orqali, qisqa muddat ichida qayta buyurtma berishingiz mumkin!\n\n**Buyurtma berish uchun - [@StrawberryHouse_bot](http://t.me/StrawebarryHouse_bot/store)**\n\nSizga doimo xizmat ko\'rsatishdan mamnunmiz!\nXurmat ila Sevara\nStrawberry House\nBarcha savollar bo\'yicha +998 55 503 5252')
         cnt += 1
         database.move_specific_row()
         await asyncio.sleep(15)
@@ -124,26 +123,27 @@ async def handler(event):
         cursor.execute('DELETE FROM responded_users')
         conn.commit()
         await event.reply("Mijozlar ro'yxati tozalandi.")
-    if not event.is_group:
-        user_id = event.sender_id
-        if is_positive_response(event.raw_text):
-            cursor.execute('SELECT user_id FROM responded_users WHERE user_id = ?', (user_id,))
-            if cursor.fetchone() is None:
-                async with client.action(event.chat_id, 'typing'):
-                    await asyncio.sleep(3)
-                    await event.reply(f'Sovg\'alar uchun mo\'ljallangan chegirmadagi mahsulotlarimiz bilan, quiydagi **"[Havola](http://t.me/StrawebarryHouse_bot/store)"**ni bosing!\n\n')
-                    cursor.execute('INSERT INTO responded_users (user_id) VALUES (?)', (user_id,))
-                    conn.commit()
-            else:
-                print(f"{user_id} ga javob berilgan")
-        else:
-            print("Negative response")
-            await event.reply(f'Axa tushinarli, agarda mahsulotlarimizni yaqinlaringizga sovg\'a sifatida xadya qilishni hohlasangiz, **"[LAUNCH](http://t.me/StrawebarryHouse_bot/store)"** knopkasini bosish orqali sotuv botimizdan foydalanishingiz mumkin!')
-            cursor.execute('INSERT INTO responded_users (user_id) VALUES (?)', (user_id,))
-            conn.commit()
+    # if not event.is_group:
+    #     user_id = event.sender_id
+    #     if is_positive_response(event.raw_text):
+    #         cursor.execute('SELECT user_id FROM responded_users WHERE user_id = ?', (user_id,))
+    #         if cursor.fetchone() is None:
+    #             async with client.action(event.chat_id, 'typing'):
+    #                 await asyncio.sleep(3)
+    #                 await event.reply(f'Sovg\'alar uchun mo\'ljallangan chegirmadagi mahsulotlarimiz bilan, quiydagi **"[Havola](http://t.me/StrawebarryHouse_bot/store)"**ni bosing!\n\n')
+    #                 cursor.execute('INSERT INTO responded_users (user_id) VALUES (?)', (user_id,))
+    #                 conn.commit()
+    #         else:
+    #             print(f"{user_id} ga javob berilgan")
+    #     else:
+    #         print("Negative response")
+    #         await event.reply(f'Axa tushinarli, agarda mahsulotlarimizni yaqinlaringizga sovg\'a sifatida xadya qilishni hohlasangiz, **"[LAUNCH](http://t.me/StrawebarryHouse_bot/store)"** knopkasini bosish orqali sotuv botimizdan foydalanishingiz mumkin!')
+    #         cursor.execute('INSERT INTO responded_users (user_id) VALUES (?)', (user_id,))
+    #         conn.commit()
     else:
         print("Unknown command")
 
-
+client.start()
+client.run_until_disconnected()
 
 
